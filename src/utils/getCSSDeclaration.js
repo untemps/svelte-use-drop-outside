e7f75c7f53ebc03af93c0d04eeb039c1a3adf49b
untemps <v.lebadezet@untemps.net>
2022-08-23 08:20:04 +0200

    test: Add test for getCSSDeclaration util, #4

--- src/utils/getCSSDeclaration.js
+++ src/utils/getCSSDeclaration.js
@@ -1,8 +1,9 @@
 export const getCSSDeclaration = (className, returnText = false) => {
-	className = className.startsWith('.') ? className : `.${className}`
-	if (!!document.styleSheets?.length) {
-		for (let { cssRules } of document.styleSheets) {
-			if (!!cssRules?.length) {
+	if (!!className) {
+		className = className.startsWith('.') ? className : `.${className}`
+
+		if (!!document.styleSheets?.length) {
+			for (let { cssRules } of document.styleSheets) {
 				for (let { selectorText, style } of cssRules) {
 					if (selectorText === className && !!style) {
 						return returnText ? style.cssText : style
